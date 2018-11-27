55fc8c909eea305a753136fb3b71b19b265bb6e0
Iglioth <Iglioth@gmail.com>
2018-11-27 15:18:45 +0100

    TV

--- FUN opdrachten/TVCollecties/Form1.cs
+++ FUN opdrachten/TVCollecties/Form1.cs
@@ -8,13 +8,80 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace TVCollecties
 {
-    public partial class Form1 : Form
+    public partial class FTV : Form
     {
-        public Form1()
+        private TV Tv = new TV(false, 1);
+        public FTV()
         {
             InitializeComponent();
+            
+        }
+      
+        private List<Channel> Channels = new List<Channel>();
+       
+
+        private void BAdd_Click(object sender, EventArgs e)
+        {
+            
+            Channels.Add(new Channel(TBAdd.Text));
+            
+        }
+
+        private void BPower_Click(object sender, EventArgs e)
+        {
+            Tv.TVStateChange();
+            LON.Visible = Tv.On;
+        }
+
+        private void BInfo_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void BRight_Click(object sender, EventArgs e)
+        {
+            Tv.ActiveChannel = Tv.ActiveChannel + 1;
+            ChannelError();
+            ChannelInfo();
+        }
+
+        private void BLeft_Click(object sender, EventArgs e)
+        {
+            Tv.ActiveChannel = Tv.ActiveChannel - 1;
+            ChannelError();
+            ChannelInfo();
+        }
+
+        private void ChannelInfo()
+        {
+            int Y = Tv.ActiveChannel - 1;
+            LCurrentChannel.Text = "Current channel: " + Tv.ActiveChannel.ToString();
+            LName.Text = "Name: " + Channels[Y].ChannelName;
+        }
+
+        private void ChannelError()
+        {
+            if (Tv.ActiveChannel > Channels.Count)
+            {
+                Tv.ActiveChannel = Tv.ActiveChannel - 1;
+                LError.Text = "Maximum Channel Reached";
+                LError.Visible = true;
+            }
+
+            else if(Tv.ActiveChannel < 1)
+            {
+                Tv.ActiveChannel = Tv.ActiveChannel + 1;
+                LError.Text = "Minimal Channel Reached";
+                LError.Visible = true;
+            }
+            else
+            {
+                LError.Visible = false;
+            }
         }
     }
 }
