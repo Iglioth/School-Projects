844f6d402741633ef3f66a07aa6fa22e552387ba
Iglioth <43240257+Iglioth@users.noreply.github.com>
2018-12-04 22:16:00 +0100

    Idk

---
+++ FUN opdrachten/TV/Channel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TV
+{
+    public class Channel
+    {
+        private string Name { get; set; }
+        //private Image ChannelImage { get; set; }
+        
+        public Channel(string name)
+        {
+            this.Name = name;
+            //this.ChannelImage = channelimage;
+        }
+
+    }
+
+    
+
+
+}
---
+++ FUN opdrachten/TV/TV.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV
+{
+    class TV
+    {
+        private bool On { get; set; }
+        private int CurrentChannel;
+
+        public TV(bool on, int currentchannel)
+        {
+            this.On = on;
+            this.CurrentChannel = currentchannel;
+        }
+    }
+}
